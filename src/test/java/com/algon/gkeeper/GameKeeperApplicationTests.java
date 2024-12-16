3612bdc5a3bc053a24d571fff89c86efbfee108e
kinderproc <kinderproc84@yandex.ru>
2024-12-16 23:43:34 +0300

    feat: configuration classes for kafka, kafka-streams and websocket implemented

--- src/test/java/com/algon/gkeeper/GameKeeperApplicationTests.java
+++ src/test/java/com/algon/gkeeper/GameKeeperApplicationTests.java
@@ -1,13 +1,21 @@
 package com.algon.gkeeper;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.socket.WebSocketHandler;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class GameKeeperApplicationTests {
 
+	@Autowired
+	WebSocketHandler webSocketHandler;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(webSocketHandler);
 	}
 
 }
